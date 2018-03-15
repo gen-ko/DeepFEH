6030425d678d36a4d199f55f096b0ec100252824
Yuan Liu <yuanl5@andrew.cmu.edu>
2018-03-15 13:33:23 -0400

    mod

--- map.py
+++ map.py
@@ -2,6 +2,7 @@
 
 
 import numpy as np
+from unit import Unit
 
 class Map(object):
     def __init__(self, ncols=6, nrows=8, nunits_a=4, nunits_b=4):
@@ -15,7 +16,13 @@ class Map(object):
                            [0, 0, 0, 0, 0, 0],
                            [0, 0, 0, 0, 0, 0],
                            [0, 0, 0, 0, 0, 0]])
-        self.units_a = {(0,0): None, (0,0):None, (0,0):None, (0,0): None}
-        self.units_b = {(0,0): None, (0,0):None, (0,0):None, (0,0): None}
+        self.units_a = {Unit(): (0, 0), Unit(): (1, 0), Unit(): (2, 0), Unit(): (3, 0)}
+        self.units_b = {Unit(): {0, 7}, Unit(): (1, 7), Unit(): (2, 7), Unit(): (3, 8)}
 
-    def
+    def get_action(self, a: Unit):
+        location = self.units
+
+
+    def move(self, a: Unit):
+
+    def movable(self, a: Unit):
