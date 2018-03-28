5e9d7e07350590e29887d16ba30de12f5b71a4ae
Shijie Wu <shijiew@andrew.cmu.edu>
2018-03-28 11:25:59 -0400

    Update trainer to compact with difficulty API

--- Linearagent.py
+++ Linearagent.py
@@ -3,6 +3,6 @@ import DQN_implementation as dqn
 identifier = "FEH linear agent"
 
 # start training
-dqn.main(identifier=identifier, model_name="linear", max_iteration=2000, epsilon=1,
+dqn.main(identifier=identifier, model_name="linear", max_iteration=20000, epsilon=1,
          epsilon_decay=0.00475, epsilon_min=0.05, interval_iteration=100, gamma=1, test_size=50,
-         learning_rate=0.00005, use_replay_memory=True, memory_size=50000, burn_in=10000)
+         learning_rate=0.0005, use_replay_memory=True, memory_size=50000, burn_in=10000, difficulty=0.05)
--- MLPagent.py
+++ MLPagent.py
@@ -5,4 +5,4 @@ identifier = "FEH MLP agent"
 # start training
 dqn.main(identifier=identifier, model_name="MLP", max_iteration=200, epsilon=1,
          epsilon_decay=0.00475, epsilon_min=0.05, interval_iteration=10, gamma=1, test_size=50,
-         learning_rate=0.00005, use_replay_memory=True, memory_size=50000, burn_in=10000)
+         learning_rate=0.00005, use_replay_memory=True, memory_size=50000, burn_in=10000, difficulty=0.05)
